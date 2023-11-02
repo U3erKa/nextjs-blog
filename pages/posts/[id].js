6a8666d38f6cc95c9d9b226a06a5522e12fe98b5
Viktor Stepanov <vitya.zap@gmail.com>
2023-11-03 00:04:34 +0200

    feat: improve pages layout

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -39,12 +41,13 @@ export default function Post({ postData }) {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <section>
-        {postData.id}
-        <br />
-        {postData.date}
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </section>
+      </article>
     </Layout>
   );
 }
