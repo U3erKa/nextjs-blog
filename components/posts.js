0091a514ea90492df785a060e49af6e7a8a63f28
Viktor Stepanov <vitya.zap@gmail.com>
2023-11-03 22:48:42 +0200

    feat: add api route example

--- components/posts.js
+++ components/posts.js
@@ -1,9 +1,9 @@
 import useSWR from 'swr';
 
 export default function Profile() {
-  const { data, error, isLoading } = useSWR('/api/user', fetch);
+  const { data, error, isLoading } = useSWR('/api/hello', fetch);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
-  return <div>hello {data.name}!</div>;
+  return <div>{data.text}</div>;
 }
